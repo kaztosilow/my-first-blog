b0c141ba11aac4daab5c0031066835c37dd2970f
kaz <kaz0108gem@gmail.com>
2019-11-25 14:15:01 +0900

    class Post modyfied

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model): #Post　はモデルの名前、models.ModelはDjango 
     title = models.CharField(max_length=200) #文字数が制限されたテキストを定義するフィールド
     text = models.TextField() #これは制限無しの長いテキスト用です。ブログポストのコンテンツ用フィールド
     created_date = models.DateTimeField(default=timezone.now) #日時のフィールド
-    publiched_date = models.DateTimeField(blank=True, null=True) #日時のフィールド
+    published_date = models.DateTimeField(blank=True, null=True) #日時のフィールド
 
     def publish(self): #ファンクション（関数）/メソッド
         self.published_date = timezone.now()
